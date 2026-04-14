fc6753e38023e41a53381e0ca031ba98edfecc99
sreenivas reddy <daramsreenivas8106@gamil.com>
2026-04-14 17:12:37 +0530

    update index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express=require('express')
+const express = require('express');
 require('dotenv').config();
 
-const app= express();
-app.get("/",(req,res)=> {
-    res.send(`<h1 color= 'red'> Hello welcome to  express js</h1> <br> <h5> text here </h5>`)
-})
+const app = express();
 
-const PORT=8080
-app.listen(PORT,'0.0.0.0' ,()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.get("/", (req, res) => {
+    res.send(`<h1>Hello welcome to express js</h1><br><h5>text here</h5>`);
+});
 
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
