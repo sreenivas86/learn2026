284aabdda6deff31cb6e75786ea8dd46a3ebe987
sreenivas reddy <daramsreenivas8106@gamil.com>
2026-04-14 17:09:49 +0530

    update port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/",(req,res)=> {
     res.send(`<h1 color= 'red'> Hello welcome to  express js</h1> <br> <h5> text here </h5>`)
 })
 
-const PORT=5000
+const PORT=8080
 app.listen(PORT,'0.0.0.0' ,()=> {
     console.log(`Server is running on port ${PORT}`)
 })
